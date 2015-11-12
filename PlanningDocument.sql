499d6cceeb011aeba3319e4c2bbaffbb8a00870f
monroema <monroema@oregonstate.edu>
2015-11-11 20:42:31 -0700

    Added planning document
    Set up responsibilities

---
+++ PlanningDocument.sql
@@ -0,0 +1,56 @@
+
+-- account types 
+checking -- no fees
+savings -- start up fee
+brokerage -- transaction fee
+mortgage -- % fee
+
+-- transaction types
+deposit
+withdrawal
+transfer
+payment
+
+if exists ( select account_type from Bi_accounts where id = 'dasjkldas' and account_type = 'brokerage' )
+	begin 
+	--create 5 $ fee
+	end 
+	
+week 7
+	create test accounts/fee types/users/ etc... --matt
+	Finish all queries 
+			-- make deposit
+			sql --Trent
+			html --Trent
+			-- make withdrawal
+			sql --Trent
+			html -- Trent
+			-- pay another account 
+			sql -- Trent
+			html -- Trent
+			-- view account summary
+			sql --Matt
+			html -- Matt
+			-- deativate account
+			sql --Matt
+			html -- Matt
+			-- open account
+			sql done -- verify
+			html -- Matt
+			-- create a user
+			sql done --verify
+			html -- Matt
+		
+	start .js files -- Your own pages
+	start .css files as needed -- Matt 
+	outline -- Trent
+	Table creation queries -- Verified w/inserts
+	
+week 8 
+
+database outline
+ER diagram of Database
+Database schema
+html
+php
+style
--- Tables/table_creation_queries.sql
+++ Tables/table_creation_queries.sql
@@ -1,10 +1,10 @@
-DROP IF EXISTS `BI_password`;
-DROP IF EXISTS `BI_account_fee`;
-DROP IF EXISTS `BI_transaction_types`;
-DROP IF EXISTS `BI_account_types`;
-DROP IF EXISTS `BI_account_transactions`;
-DROP IF EXISTS `BI_accounts`;
-DROP IF EXISTS `BI_user`;
+DROP TABLE IF EXISTS `BI_password`;
+DROP TABLE IF EXISTS `BI_account_fee`;
+DROP TABLE IF EXISTS `BI_transaction_types`;
+DROP TABLE IF EXISTS `BI_account_types`;
+DROP TABLE IF EXISTS `BI_account_transactions`;
+DROP TABLE IF EXISTS `BI_accounts`;
+DROP TABLE IF EXISTS `BI_user`;
 
 
 CREATE TABLE `BI_user`
@@ -31,18 +31,11 @@ CREATE TABLE `BI_password`
 ,FOREIGN KEY (`user_id`) REFERENCES `BI_user` (`id`)
 )ENGINE = InnoDB;
 
-CREATE TABLE `BI_accounts` (
-  id INT(11) NOT NULL AUTO_INCREMENT,
-  user_id INT(11) NOT NULL,
-  account_type_id INT(11) NOT NULL,
-  is_active TINYINT(1) NOT NULL,
-  current_balance INT(11),
-  last_transaction_date DATE NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (user_id) REFERENCES BI_user (id),
-  FOREIGN KEY (account_type_id) REFERENCES BI_account_types (id)
+CREATE TABLE `BI_account_fee`
+(`id` INT(11) NOT NULL AUTO_INCREMENT
+,`amount` DECIMAL(10,2) NOT NULL 
+,PRIMARY KEY (`id`)
 )ENGINE = InnoDB; 
-
 
 CREATE TABLE `BI_account_types`
 (id INT(11) NOT NULL AUTO_INCREMENT
@@ -51,6 +44,25 @@ CREATE TABLE `BI_account_types`
 ,`fee_type_id` INT(11) NOT NULL
 ,PRIMARY KEY (`id`)
 ,FOREIGN KEY (`fee_type_id`) REFERENCES `BI_account_fee` (`id`)
+)ENGINE = InnoDB; 
+
+CREATE TABLE `BI_accounts` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `user_id` INT(11) NOT NULL,
+  `account_type_id` INT(11) NOT NULL,
+  `is_active` TINYINT(1) NOT NULL,
+  `current_balance` INT(11),
+  `last_transaction_date` DATE NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`user_id`) REFERENCES BI_user (`id`),
+  FOREIGN KEY (`account_type_id`) REFERENCES BI_account_types (`id`)
+)ENGINE = InnoDB; 
+
+
+CREATE TABLE `BI_transaction_types`
+(`id` INT(11) NOT NULL AUTO_INCREMENT
+,`transaction_type` VARCHAR(20) NOT NULL
+,PRIMARY KEY (`id`)
 )ENGINE = InnoDB; 
 
 CREATE TABLE `BI_account_transactions`
@@ -67,18 +79,5 @@ CREATE TABLE `BI_account_transactions`
   PRIMARY KEY (id),
   FOREIGN KEY (payee_account_id) REFERENCES BI_accounts (id),
   FOREIGN KEY (payer_account_id) REFERENCES BI_accounts (id),
+  FOREIGN KEY (transaction_type_id) REFERENCES BI_transaction_types (id)
 )ENGINE = InnoDB;
-
-CREATE TABLE `BI_account_fee`
-(`id` INT(11) NOT NULL AUTO_INCREMENT
-,`amount` DECIMAL(10,2) NOT NULL 
-,PRIMARY KEY (`id`)
-)ENGINE = InnoDB; 
-
-
-CREATE TABLE `BI_transaction_types`
-(`id` INT(11) NOT NULL AUTO_INCREMENT
-,`transaction_type` VARCHAR(20) NOT NULL
-,PRIMARY KEY (`id`)
-)ENGINE = InnoDB; 
-
