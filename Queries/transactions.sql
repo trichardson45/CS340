3c02567dff743120d9d95566cae452c31579276b
Trent Richardson <wherestrent@gmail.com>
2015-11-13 16:32:03 -0700

    transaction queries - rough draft

---
+++ Queries/transactions.sql
@@ -0,0 +1,33 @@
+--these are very basic to see how the account updates work.  it seems to work, but need to check references
+--to make sure all tables are called and updated accordingly.  obviously, all real numbers will come from user
+
+--deposit
+UPDATE BI_accounts
+SET current_balance = current_balance + 101.35
+WHERE id = 1;
+
+INSERT INTO BI_account_transactions (payee_account_id, payer_account_id, amount, transaction_date, transaction_type_id, memo, posting_date, isVoid)
+VALUES (1, 1, +101.35, CURRENT_TIMESTAMP, 1, "Testing, 1,2,3", CURRENT_TIMESTAMP, 0);
+
+--withdrawl
+UPDATE BI_accounts
+SET current_balance = current_balance - 100.50
+WHERE id = 7;
+
+INSERT INTO BI_account_transactions (payee_account_id, payer_account_id, amount, transaction_date, transaction_type_id, memo, posting_date, isVoid)
+VALUES (7, 7, -100.50, CURRENT_TIMESTAMP, 1, "Testing, 1,2,3", CURRENT_TIMESTAMP, 0);
+
+--transfer
+UPDATE BI_accounts
+SET current_balance = current_balance - 100.50
+WHERE id = 3;
+
+INSERT INTO BI_account_transactions (payee_account_id, payer_account_id, amount, transaction_date, transaction_type_id, memo, posting_date, isVoid)
+VALUES (5, 3, -100.50, CURRENT_TIMESTAMP, 1, "Testing, 1,2,3", CURRENT_TIMESTAMP, 0);
+
+UPDATE BI_accounts
+SET current_balance = current_balance + 100.50
+WHERE id = 5;
+
+INSERT INTO BI_account_transactions (payee_account_id, payer_account_id, amount, transaction_date, transaction_type_id, memo, posting_date, isVoid)
+VALUES (5, 3, +100.50, CURRENT_TIMESTAMP, 1, "Testing, 1,2,3", CURRENT_TIMESTAMP, 0);
