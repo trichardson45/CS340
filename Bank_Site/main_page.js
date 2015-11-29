299678a0848418efea64fc6fecd8376be2c48d50
Trent Richardson <wherestrent@gmail.com>
2015-11-29 16:13:59 -0700

    g

--- Bank_Site/main_page.js
+++ Bank_Site/main_page.js
@@ -90,7 +90,7 @@ app.get('/', function (req, res, next) {
         })
 
     } else if (req.query.depositAmt != 0 && req.query.depositAmt != '' && req.query.depositAmt != null) {
-      var newquery6 = "UPDATE BI_accounts SET `current_balance` = `current_balance`+" + String(req.query.depositAmt) + " WHERE `user_id` =" + curId + "AND `account_type_id`=";
+      var newquery6 = "UPDATE BI_accounts SET `current_balance` = `current_balance`+" + String(req.query.depositAmt) + " WHERE `user_id` =" + curId + " AND `account_type_id`=";
       newquery6 += "(SELECT `id` FROM BI_account_types WHERE `type_name` ='" + req.query.accountType + "')";
       console.log(newquery6);
       /*mysql.pool.query(newquery6, function (err, rows){
@@ -98,7 +98,7 @@ app.get('/', function (req, res, next) {
           next(err);
           return;
         }
-        
+
       })*/
       res.render('main_page', context);
     }
