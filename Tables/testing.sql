a0af38b5a88abb0492b61d2f3fe34992bb6cb22b
Trent Richardson <wherestrent@gmail.com>
2015-12-01 19:27:49 -0700

    all transactions done

--- Tables/testing.sql
+++ Tables/testing.sql
@@ -95,3 +95,67 @@ VALUES ((SELECT `id` FROM `BI_user` WHERE `username` = 'richardstr')
 ,900000
 ,CURRENT_TIMESTAMP);
 
+INSERT INTO `BI_user` (`f_name`,`m_name`,`l_name`,`email`,`username`,`is_active`)
+values('Jack','','Daniels','jack@jackdaniels.com','jack_daniels',1);
+
+INSERT INTO `BI_password` (`user_id`,`is_active`,`password`)
+VALUES ((SELECT `id` FROM `BI_user` WHERE `username` = 'jack_daniels'),1,'1234');
+
+INSERT INTO `BI_accounts` (`user_id`,`account_type_id`,`is_active`,`current_balance`,`last_transaction_date`)
+VALUES ((SELECT `id` FROM `BI_user` WHERE `username` = 'jack_daniels') 
+,(SELECT `id` FROM `BI_account_types` WHERE `type_name` = 'Checking')
+,1
+,0
+,CURRENT_TIMESTAMP);
+
+INSERT INTO `BI_user` (`f_name`,`m_name`,`l_name`,`email`,`username`,`is_active`)
+values('Sierra','','Nevada','beer@sierranevada.com','sierra_nevada',1);
+
+INSERT INTO `BI_password` (`user_id`,`is_active`,`password`)
+VALUES ((SELECT `id` FROM `BI_user` WHERE `username` = 'sierra_nevada'),1,'1234');
+
+INSERT INTO `BI_accounts` (`user_id`,`account_type_id`,`is_active`,`current_balance`,`last_transaction_date`)
+VALUES ((SELECT `id` FROM `BI_user` WHERE `username` = 'sierra_nevada') 
+,(SELECT `id` FROM `BI_account_types` WHERE `type_name` = 'Checking')
+,1
+,0
+,CURRENT_TIMESTAMP);
+
+INSERT INTO `BI_user` (`f_name`,`m_name`,`l_name`,`email`,`username`,`is_active`)
+values('Quicken','','Loans','loans@quickenloans.com','quicken_loans',1);
+
+INSERT INTO `BI_password` (`user_id`,`is_active`,`password`)
+VALUES ((SELECT `id` FROM `BI_user` WHERE `username` = 'quicken_loans'),1,'1234');
+
+INSERT INTO `BI_accounts` (`user_id`,`account_type_id`,`is_active`,`current_balance`,`last_transaction_date`)
+VALUES ((SELECT `id` FROM `BI_user` WHERE `username` = 'quicken_loans') 
+,(SELECT `id` FROM `BI_account_types` WHERE `type_name` = 'Checking')
+,1
+,0
+,CURRENT_TIMESTAMP);
+
+INSERT INTO `BI_user` (`f_name`,`m_name`,`l_name`,`email`,`username`,`is_active`)
+values('Xcel','','Energy','energy@xcelenergy.com','xcel_energy',1);
+
+INSERT INTO `BI_password` (`user_id`,`is_active`,`password`)
+VALUES ((SELECT `id` FROM `BI_user` WHERE `username` = 'xcel_energy'),1,'1234');
+
+INSERT INTO `BI_accounts` (`user_id`,`account_type_id`,`is_active`,`current_balance`,`last_transaction_date`)
+VALUES ((SELECT `id` FROM `BI_user` WHERE `username` = 'xcel_energy') 
+,(SELECT `id` FROM `BI_account_types` WHERE `type_name` = 'Checking')
+,1
+,0
+,CURRENT_TIMESTAMP);
+
+INSERT INTO `BI_user` (`f_name`,`m_name`,`l_name`,`email`,`username`,`is_active`)
+values('Chase','','Bank','chase@chasebank.com','chase_bank',1);
+
+INSERT INTO `BI_password` (`user_id`,`is_active`,`password`)
+VALUES ((SELECT `id` FROM `BI_user` WHERE `username` = 'chase_bank'),1,'1234');
+
+INSERT INTO `BI_accounts` (`user_id`,`account_type_id`,`is_active`,`current_balance`,`last_transaction_date`)
+VALUES ((SELECT `id` FROM `BI_user` WHERE `username` = 'chase_bank') 
+,(SELECT `id` FROM `BI_account_types` WHERE `type_name` = 'Checking')
+,1
+,0
+,CURRENT_TIMESTAMP);
